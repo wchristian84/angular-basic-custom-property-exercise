fa04abf0552e25e2befe228a4b8fb90d94f3589a
wchristian84 <wade.christian@gmail.com>
2022-10-04 12:51:38 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-basic-custom-property-exercise';
+export class AppComponent implements OnInit {
+  name = 'Wade';
+
+  ngOnInit(): void {
+  }
+
 }
